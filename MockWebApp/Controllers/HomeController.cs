ba82a0361f8473c74c23eb6b1af600fcc7d62248
Mogamad Tayieb Kamaldien <kamaldientayieb@gmail.com>
2024-07-01 14:51:47 +0200

    Fixed

--- MockWebApp/Controllers/HomeController.cs
+++ MockWebApp/Controllers/HomeController.cs
@@ -30,6 +30,12 @@ namespace MockWebApp.Controllers
         {
             return View();
         }
+        public IActionResult BodyPartSelect()
+        {
+            return View();
+        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
