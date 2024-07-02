461709d5e45e61f6d0f7539e9f68ff4199a24869
erika21akire <5100162@mypillar7uwc.ac.za>
2024-07-02 09:39:06 +0200

    PhysioCP & Select Athlete + UI

--- MockWebApp/Controllers/HomeController.cs
+++ MockWebApp/Controllers/HomeController.cs
@@ -23,6 +23,15 @@ namespace MockWebApp.Controllers
             return View();
         }
 
+        public IActionResult PhysioCP()
+        {
+            return View();
+        }
+        public IActionResult SelectAthlete()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
