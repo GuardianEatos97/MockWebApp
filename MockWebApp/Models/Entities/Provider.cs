feec21445d85e79f68031dea5f168a10b794b19b
thaverk <thaverk01@gmail.com>
2024-07-03 13:49:37 +0200

    SP Updated

--- MockWebApp/Models/Entities/Provider.cs
+++ MockWebApp/Models/Entities/Provider.cs
@@ -8,7 +8,7 @@ namespace MockWebApp.Models.Entities
         public int SPId { get; set; }
         public string SPName { get; set; }
         public string SPEmail { get; set; }
-        public string SPPassword { get; set; }
+        public string SPPassword { get; set; } 
         public int SPType { get; set; }
 
     
