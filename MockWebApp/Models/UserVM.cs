f35bf7e700b062268914da7f313dc1a48075f7d7
erika21akire <5100162@mypillar7uwc.ac.za>
2024-07-03 21:43:09 +0200

    UI and additional info

--- MockWebApp/Models/UserVM.cs
+++ MockWebApp/Models/UserVM.cs
@@ -3,7 +3,7 @@
     public class UserVM
     {
         public int Id { get; set; }
-        public string? UserPic { get; set; }
+        public string UserPic { get; set; }
         public int UserJersey { get; set; }
         public string UserName { get; set; }
         public string UserEmail { get; set; }
